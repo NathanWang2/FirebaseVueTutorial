40c15d66226ad93a7bbda5bd336cf20e11c34dd3
Nathan <nathan.wang2@gmail.com>
2019-08-11 19:19:24 -0500

    Completed the navigation bar to route to the new pages

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,13 @@
 
       <v-divider></v-divider>
 
-      <v-list nav v-for="item in menuItem" :key="item.title">
-        <v-list-item>
+      <v-list nav>
+        <v-list-item
+        v-for="item in menuItem"
+        :key="item.title"
+        router
+        :to="item.link"
+        >
           <v-list-item-icon>
             <v-icon>{{item.icon}}</v-icon>
           </v-list-item-icon>
@@ -25,25 +30,30 @@
     <v-app-bar app dark class="teal darken-4">
       <v-app-bar-nav-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up" />
       <v-toolbar flat class="teal darken-4">
-        <v-toolbar-title>Dev Meetup</v-toolbar-title>
+        <v-toolbar-title>
+          <router-link to="/" tag="span" style="cursor: pointer">Dev Meetup</router-link>
+        </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-xs-only" v-for="item in menuItem" :key="item.title">
-          <v-btn text>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn text
+            v-for="item in menuItem"
+            :key="item.title"
+            router
+            :to="item.link"
+            >
             <v-icon left>{{item.icon}}</v-icon>
             {{item.title}}
           </v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </v-app-bar>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout>
-          <v-flex text-xs>
-            <h1>HELLO</h1>
-          </v-flex>
-        </v-layout>
-      </v-container>
+    <main>
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
     </v-content>
+    </main>
   </v-app>
 </template>
 
@@ -53,11 +63,11 @@ export default {
     return {
       sideNav: false,
       menuItem: [
-        { icon: 'mdi-account-group', title: 'View Meetups' },
-        { icon: 'mdi-account', title: 'Profile' },
-        { icon: 'mdi-calendar-plus', title: 'Organize Meetup' },
-        { icon: 'mdi-account-plus', title: 'Sign up' },
-        { icon: 'mdi-lock-open', title: 'Sign in' }
+        { icon: 'mdi-account-group', title: 'View Meetups', link: '/meetups' },
+        { icon: 'mdi-calendar-plus', title: 'Organize Meetup', link: '/meetup/new' },
+        { icon: 'mdi-account', title: 'Profile', link: '/profile' },
+        { icon: 'mdi-account-plus', title: 'Sign up', link: '/signup' },
+        { icon: 'mdi-lock-open', title: 'Sign in', link: '/signin' }
       ]
     }
   }
--- src/router.js
+++ src/router.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import Meetups from '@/components/Meetup/Meetups'
+import CreateMeetup from '@/components/Meetup/CreateMeetup'
+import Profile from '@/components/User/Profile'
+import Signup from '@/components/User/Signup'
+import Signin from '@/components/User/Signin'
 
 Vue.use(Router)
 
@@ -7,5 +13,35 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/meetups',
+      name: 'Meetups',
+      component: Meetups
+    },
+    {
+      path: '/meetup/new',
+      name: 'CreateMeetup',
+      component: CreateMeetup
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    }
   ]
 })
