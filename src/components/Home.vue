867493489df0569e68cf733974790736e99bf475
Nathan Wang <nathan.wang2@gmail.com>
2019-08-13 21:28:43 -0500

    Switching work stations

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        <h1>Welcome to the home page</h1>
-    </div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12 sm6 class="text-sm-right">
+                <v-btn
+                    large
+                    router
+                    to="/meetups"
+                    class="primary">Explore Meetups</v-btn>
+            </v-flex>
+             <v-flex xs12 sm6 class="text-sm-left">
+                <v-btn
+                    large
+                    router
+                    to="/meetups"
+                    class="primary">Organize Meetup</v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
