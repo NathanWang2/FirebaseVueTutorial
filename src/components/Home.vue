9e2d600c536a2a1f94668b71ef628aecf8d253c6
Nathan Wang <nathan.wang2@gmail.com>
2019-08-17 12:27:34 -0500

    Moving to desktop

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,48 @@
 <template>
-    <v-container>
-        <v-layout row wrap>
-            <v-flex xs12 sm6 class="text-sm-right">
-                <v-btn
-                    large
-                    router
-                    to="/meetups"
-                    class="primary">Explore Meetups</v-btn>
-            </v-flex>
-             <v-flex xs12 sm6 class="text-sm-left">
-                <v-btn
-                    large
-                    router
-                    to="/meetups"
-                    class="primary">Organize Meetup</v-btn>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container grid-list-md>
+    <v-layout align-center justify-center row fill-height class="text-center">
+      <v-flex align-self-center class="text-sm-right">
+        <v-btn large router to="/meetups" class="primary">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetups" class="primary">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-carousel :show-arrows="false">
+        <v-carousel-item
+        v-for="meetup in meetups"
+        :key="meetup.id"
+        :src="meetup.imageUrl">
+        <div class="title">
+          {{meetup.title}}
+        </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/dc/Skyscrapers_of_Shinjuku_2009_January_%28revised%29.jpg', id: 'TEST', title: 'TEST MEETUP' },
+        { imageUrl: 'https://www.thebalancecareers.com/thmb/7RNbnMgqp7sHt5i7kAwvwtNKbG4=/3865x2576/filters:no_upscale():max_bytes(150000):strip_icc()/high-angle-view-of-lower-east-side-manhattan-downtown--new-york-city--usa-640006562-5ae52a273de423003776ae2e.jpg', id: 'TEST2', title: 'OTHER TEST' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title {
+    text-align: center;
+    color: black;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    background-color: #fbf7f3;
+    opacity: 0.6;
+  }
+</style>
